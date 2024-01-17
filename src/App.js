adb4e2bbb7257b600f6c6278daca27989f4dac95
syeef <syeefislam@yahoo.com>
2024-01-17 08:32:03 +0530

    overlay and modal added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
 import Header from "./components/Layouts/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
